81f77938a10907477f7aa218cd49c349817b8ec8
Tetiana1386 <tanyashostak13@gmail.com>
2022-02-07 22:22:35 +0200

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodosView from './TodosView';
+
+const App = () => {
+    return (
+        <>
+            <TodosView />
+        </>
+    )
+}
+
+export default App;
